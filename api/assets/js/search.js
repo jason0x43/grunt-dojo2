e290f8113cf0ff97d8930e97bb0e079ce37517a6
Jason Cheatham <j.cheatham@gmail.com>
2017-02-10 11:07:27 -0500

    Update API docs

---
+++ api/assets/js/search.js
@@ -0,0 +1,1 @@
+﻿var typedoc = typedoc || {};typedoc.search = typedoc.search || {};typedoc.search.data = {"kinds":{"1":"External module"},"rows":[{"id":0,"kind":1,"name":"\"typedoc\"","url":"modules/_typedoc_.html","classes":"tsd-kind-external-module"}]};
